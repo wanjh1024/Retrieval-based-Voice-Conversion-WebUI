514181a12c2b74ef97b1acfaf1869d759aa62065
wanjh1024 <75013443+wanjh1024@users.noreply.github.com>
2025-08-05 20:43:58 +0800

    Update dlmodels.sh

--- tools/dlmodels.sh
+++ tools/dlmodels.sh
@@ -58,19 +58,23 @@ check_file_special() {
   fi
 }
 
-check_file_pretrained pretrained D32k.pth
-check_file_pretrained pretrained D40k.pth
-check_file_pretrained pretrained D48k.pth
-check_file_pretrained pretrained G32k.pth
-check_file_pretrained pretrained G40k.pth
-check_file_pretrained pretrained G48k.pth
-check_file_pretrained pretrained_v2 f0D40k.pth
-check_file_pretrained pretrained_v2 f0G40k.pth
+check_file_pretrained pretrained_v2 D32k.pth
 check_file_pretrained pretrained_v2 D40k.pth
+check_file_pretrained pretrained_v2 D48k.pth
+check_file_pretrained pretrained_v2 G32k.pth
 check_file_pretrained pretrained_v2 G40k.pth
+check_file_pretrained pretrained_v2 G48k.pth
+check_file_pretrained pretrained_v2 f0D32k.pth
+check_file_pretrained pretrained_v2 f0D40k.pth
+check_file_pretrained pretrained_v2 f0D48k.pth
+check_file_pretrained pretrained_v2 f0G32k.pth
+check_file_pretrained pretrained_v2 f0G40k.pth
+check_file_pretrained pretrained_v2 f0G48k.pth
 check_file_pretrained uvr5_weights HP2_all_vocals.pth
+check_file_pretrained uvr5_weights HP2-人声vocals+非人声instrumentals.pth
 check_file_pretrained uvr5_weights HP3_all_vocals.pth
 check_file_pretrained uvr5_weights HP5_only_main_vocal.pth
+check_file_pretrained uvr5_weights HP5-主旋律人声vocals+其他instrumentals.pth
 check_file_pretrained uvr5_weights VR-DeEchoAggressive.pth
 check_file_pretrained uvr5_weights VR-DeEchoDeReverb.pth
 check_file_pretrained uvr5_weights VR-DeEchoNormal.pth
